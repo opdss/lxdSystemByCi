c78f75f465e7df1a644352f5a7a93737dd0fb349
opdss <opdss@qq.com>
2015-05-10 11:48:26 +0800

    update

---
+++ lxdSystem/views/public/index.php
@@ -0,0 +1,25 @@
+<!-- main container -->
+<div class="content">
+
+    <div id="pad-wrapper">
+
+        <div class="separator"></div>
+        <div class="section">
+            <!-- alerts -->
+            <h4 class="title">Welcome</h4>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="alert alert-info">
+                        <i class="icon-exclamation-sign"></i>
+                        有问题请及时联系 好人阿新:opdss@qq.com
+                    </div>
+                    <div class="alert alert-success">
+                        <i class="icon-ok-sign"></i> 您已登录成功
+                    </div>
+                </div>
+            </div>
+            <!-- end alerts -->
+        </div>
+
+    </div>
+</div>
