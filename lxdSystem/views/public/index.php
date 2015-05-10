6a280d915d9b722d8da3c3c556d4a615c9d455cb
opdss <opdss@qq.com>
2015-05-10 12:24:28 +0800

    update

--- lxdSystem/views/public/index.php
+++ lxdSystem/views/public/index.php
@@ -11,7 +11,7 @@
                 <div class="col-md-6">
                     <div class="alert alert-info">
                         <i class="icon-exclamation-sign"></i>
-                        有问题请及时联系 好人阿新:opdss@qq.com
+                        有问题请及时联系 好人阿新:opdss@qq.com; 奇奇:vankizou@163.com; 金总:1441059672@qq.com; 小威:1109788528@qq.com
                     </div>
                     <div class="alert alert-success">
                         <i class="icon-ok-sign"></i> 您已登录成功
