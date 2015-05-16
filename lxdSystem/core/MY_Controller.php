e5f5617f4053ec8db463e412477634067d36a5ec
opdss <opdss@qq.com>
2015-05-16 19:37:32 +0800

    update

--- lxdSystem/core/MY_Controller.php
+++ lxdSystem/core/MY_Controller.php
@@ -49,7 +49,7 @@ class MY_Controller extends CI_Controller {
 		$head = array(
 			'css'   => array_merge($this->style['css'], $this->restyle['css']),
 			'js'    => array_merge($this->style['js'], $this->restyle['js']),
-			'title' => isset($data['title'])?$data['title']:($this->MENU[$this->router->class]['submenu'][$this->router->method].'-隆鑫达后台管理系统')
+			'title' => isset($data['title'])?$data['title']:(isset($this->MENU[$this->router->class]['submenu'][$this->router->method])?$this->MENU[$this->router->class]['submenu'][$this->router->method].COPYRIGHT:COPYRIGHT)
 		);
 		$this->load->view('public/header', $head);
 		if (isset($this->MENU)) {
